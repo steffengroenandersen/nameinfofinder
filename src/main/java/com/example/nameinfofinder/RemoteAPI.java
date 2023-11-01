436ad0f80505ac1c78c2ee739cef4821b50efd9c
steffengroenandersen <steffengroenandersen@gmail.com>
2023-11-01 21:18:23 +0100

    Add new approach to getFullDetailsForName

--- src/main/java/com/example/nameinfofinder/RemoteAPI.java
+++ src/main/java/com/example/nameinfofinder/RemoteAPI.java
@@ -1,14 +1,13 @@
 package com.example.nameinfofinder;
 
 import com.example.nameinfofinder.dto.AgeResponse;
+import com.example.nameinfofinder.dto.CombinedNameResponse;
 import com.example.nameinfofinder.dto.GenderResponse;
 import com.example.nameinfofinder.dto.NationalityResponse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.sql.SQLOutput;
 
 @Configuration
 public class RemoteAPI implements CommandLineRunner {
@@ -23,7 +22,6 @@ public class RemoteAPI implements CommandLineRunner {
                 .bodyToMono(GenderResponse.class)
                 .doOnError(e -> System.out.println("UUUPS : " + e.getMessage()));
 
-
         return genderResponseMono;
     }
 
@@ -36,7 +34,6 @@ public class RemoteAPI implements CommandLineRunner {
                 .retrieve()
                 .bodyToMono(AgeResponse.class)
                 .doOnError(e -> System.out.println("UUUPS : " + e.getMessage()));
-
 
         return ageResponseMono;
     }
@@ -54,34 +51,55 @@ public class RemoteAPI implements CommandLineRunner {
         return nationalityResponseMono;
     }
 
-    // Call three endpoints
+    // Call three endpoints and print combined results
     public void getFulLDetailsFromName(String name) {
-
+        // Create Mono instances for each of the remote API calls
         Mono<GenderResponse> genderResponseMono = getGenderForName(name);
         Mono<AgeResponse> ageResponseMono = getAgeForName(name);
         Mono<NationalityResponse> nationalityResponseMono = getNationalityForName(name);
 
+        // Combine the results of the Mono instances into a single Mono
         Mono<String> combinedResult = Mono.zip(
-                genderResponseMono, 
-                ageResponseMono, 
+                genderResponseMono,
+                ageResponseMono,
                 nationalityResponseMono
         ).map(results -> {
-           String gender = results.getT1().getGender();
-           int age = results.getT2().getAge();
-           String country = results.getT3().getCountry().get(0).getCountry_id();
-           
-           return "Name: " + name + ", Gender: " + gender + ", Age: " + age + ", Nationality: " + country;
+            // Extract gender, age, and country information from the results
+            String gender = results.getT1().getGender();
+            int age = results.getT2().getAge();
+            String country = results.getT3().getCountry().get(0).getCountry_id();
+
+            // Create a formatted string with the full details
+            return "Name: " + name + ", Gender: " + gender + ", Age: " + age + ", Nationality: " + country;
         });
 
+        // Subscribe to the combinedResult to perform an action when the result is available
         combinedResult.subscribe(result -> System.out.println(result));
+    }
+
+    // Call three endpoints and print combined results using CombinedNameResponse
+    public void getFullDetailsForNameV2(String name) {
+        Mono<GenderResponse> genderResponseMono = getGenderForName(name);
+        Mono<AgeResponse> ageResponseMono = getAgeForName(name);
+        Mono<NationalityResponse> nationalityResponseMono = getNationalityForName(name);
+
+        Mono<CombinedNameResponse> combinedNameResponseMono = Mono.zip(
+                genderResponseMono,
+                ageResponseMono,
+                nationalityResponseMono
+        ).map(tuple3 -> new CombinedNameResponse(
+                name,
+                tuple3.getT1().getGender(),
+                tuple3.getT2().getAge(),
+                tuple3.getT3().getCountry().get(0).getCountry_id()
+        ));
+
+        // Subscribe to the combinedNameResponseMono to print the result
+        combinedNameResponseMono.subscribe(result -> System.out.println(result));
     }
 
     @Override
     public void run(String... args) throws Exception {
-
-        //Mono<Gender> resultForParker = getGenderForName("parker");
-        //System.out.println(resultForParker.block().getGender());
-
         String name = "Steffen";
 
         // Get name for Steffen
@@ -96,9 +114,8 @@ public class RemoteAPI implements CommandLineRunner {
         Mono<NationalityResponse> nationalityResultForSteffen = getNationalityForName(name);
         System.out.println(nationalityResultForSteffen.block().getCountry().size());
 
-        // Get full details for Steffen
+        // Get full details for Steffen using two different approaches
         getFulLDetailsFromName(name);
+        getFullDetailsForNameV2(name);
     }
-
-
 }
--- src/main/java/com/example/nameinfofinder/dto/CombinedNameResponse.java
+++ src/main/java/com/example/nameinfofinder/dto/CombinedNameResponse.java
@@ -1,5 +1,6 @@
 package com.example.nameinfofinder.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,9 +8,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CombinedNameResponse {
     private String name;
-    private int age;
     private String gender;
+    private int age;
     private String country;
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Gender: " + gender + ", Age: " + age + ", Nationality: " + country;
+    }
 }

e050a9f77da5058098bc742cac55c315b9776232
steffengroenandersen <steffengroenandersen@gmail.com>
2023-11-01 20:56:24 +0100

    Add getFullDetailsForName method

--- src/main/java/com/example/nameinfofinder/RemoteAPI.java
+++ src/main/java/com/example/nameinfofinder/RemoteAPI.java
@@ -20,7 +20,9 @@ public class RemoteAPI implements CommandLineRunner {
         Mono<GenderResponse> genderResponseMono = webClient.get()
                 .uri("https://api.genderize.io?name=" + name)
                 .retrieve()
-                .bodyToMono(GenderResponse.class);
+                .bodyToMono(GenderResponse.class)
+                .doOnError(e -> System.out.println("UUUPS : " + e.getMessage()));
+
 
         return genderResponseMono;
     }
@@ -28,26 +30,50 @@ public class RemoteAPI implements CommandLineRunner {
     // Get Age for name
     Mono<AgeResponse> getAgeForName(String name) {
         WebClient webClient = WebClient.create();
-        
+
         Mono<AgeResponse> ageResponseMono = webClient.get()
                 .uri("https://api.agify.io?name=" + name)
                 .retrieve()
-                .bodyToMono(AgeResponse.class);
-        
+                .bodyToMono(AgeResponse.class)
+                .doOnError(e -> System.out.println("UUUPS : " + e.getMessage()));
+
+
         return ageResponseMono;
     }
 
     // Get Nationality for name
-    
-    Mono<NationalityResponse> getNationalityForName(String name){
+    Mono<NationalityResponse> getNationalityForName(String name) {
         WebClient webClient = WebClient.create();
-        
+
         Mono<NationalityResponse> nationalityResponseMono = webClient.get()
                 .uri("https://api.nationalize.io?name=" + name)
                 .retrieve()
-                .bodyToMono(NationalityResponse.class);
-        
+                .bodyToMono(NationalityResponse.class)
+                .doOnError(e -> System.out.println("UUUPS : " + e.getMessage()));
+
         return nationalityResponseMono;
+    }
+
+    // Call three endpoints
+    public void getFulLDetailsFromName(String name) {
+
+        Mono<GenderResponse> genderResponseMono = getGenderForName(name);
+        Mono<AgeResponse> ageResponseMono = getAgeForName(name);
+        Mono<NationalityResponse> nationalityResponseMono = getNationalityForName(name);
+
+        Mono<String> combinedResult = Mono.zip(
+                genderResponseMono, 
+                ageResponseMono, 
+                nationalityResponseMono
+        ).map(results -> {
+           String gender = results.getT1().getGender();
+           int age = results.getT2().getAge();
+           String country = results.getT3().getCountry().get(0).getCountry_id();
+           
+           return "Name: " + name + ", Gender: " + gender + ", Age: " + age + ", Nationality: " + country;
+        });
+
+        combinedResult.subscribe(result -> System.out.println(result));
     }
 
     @Override
@@ -65,13 +91,13 @@ public class RemoteAPI implements CommandLineRunner {
         // Get age for Steffen
         Mono<AgeResponse> ageResultForSteffen = getAgeForName(name);
         System.out.println(ageResultForSteffen.block().getAge());
-        
+
         // Get nationality for Steffen
         Mono<NationalityResponse> nationalityResultForSteffen = getNationalityForName(name);
         System.out.println(nationalityResultForSteffen.block().getCountry().size());
-                
 
-
+        // Get full details for Steffen
+        getFulLDetailsFromName(name);
     }
 
 
